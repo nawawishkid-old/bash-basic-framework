340953b8fd8506d85591b661ec3cf04c08c23233
Nawawish <hydrokidz@gmail.com>
2018-06-12 19:45:35 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,3 +10,24 @@ DOC_DIR="${APP_SOURCE_DIR}/doc"
 # Import dependencies
 source ${APP_SOURCE_DIR}/utils.sh
 import "output"
+
+# If no argument given, echo help text
+test empty $1 --txec "help main" --txit
+
+while [ $# -ne 0 ]
+do
+    case "$1" in
+        1)
+            echo 1
+        ;;
+        2|3)
+            echo 2 or 3
+        ;;
+        *)
+            echo default
+        ;;
+    esac
+    
+    shift
+    
+done
